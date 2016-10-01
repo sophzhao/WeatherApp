6c5438c56a7600d41f32e029f0b2449738a66197
Sophie Zhao <syzhao@andrew.cmu.edu>
2016-10-01 01:36:00 -0400

    initial commit

--- ajax_calls.js
+++ ajax_calls.js
@@ -1,20 +1,60 @@
-jQuery(document).ready(function($) {
-  $.ajax({
-      // https API request for jsonp data
-      url : "https://api.wunderground.com/api/2e568790d4726776/geolookup/conditions/q/MA/Lexington.json",
-      dataType : "jsonp",
-      // if parsed_json / jsonp is returned, continue with this function
-      success : function(parsed_json) {
-          // relevant values stored in variables 
-          var location = parsed_json['location']['city'];
-          var state = parsed_json['location']['state'];
-          var temp_f = parsed_json['current_observation']['temp_f'];
-          // write the data from API into HTML using div id's
-          $("#infoArea").html("<p>Current temperature in " + location + " " + state + " is: </p>");
-          $("#temp").html("<p>" + temp_f + "°F</p>")
-      }
+jQuery(document).ready(function() {
+ 
+
+
+  $.getJSON("cities.json", function(responseObject, diditwork) {       
+
+    $('input.autocomplete').autocomplete({
+       data: responseObject
+    });      
+  });  // getJSON
+
+
+
+  $.getJSON("states.json", function(responseObject, diditwork) {       
+    var displayOptions = "<option value='' disabled selected>State</option>"
+    for (var i = 0; i<responseObject.states.length; i++) {
+       displayOptions +=  "<option value=" + responseObject.states[i].abbreviation+  ">" + responseObject.states[i].name + "</option>";
+    }
+
+    $('#state_select').html(displayOptions); 
+    //makes the select work with materilize
+    $('select').material_select();       
+  });  // getJSON
+
+
+  $('input').keypress(function(e) {
+    if (e.which == 13) {
+      findTemperature($('select').val(), $('#autocomplete-input').val());
+    }
   });
+
+
+  $('span.lever').click(function() {
+     // let ($('#').prop('checked'))
+     let idCard = $(this).parent().parent().attr('id');
+     let checked = $('#check'+idCard).prop('checked');
+
+     if (checked) {
+        $('#h2'+idCard).text($('#'+idCard).data('f')+"°F");
+     }
+     else {
+        $('#h2'+idCard).text($('#'+idCard).data('c')+"°C");
+     }
+
+  });
+
+  $('a').click(function() {
+     // let ($('#').prop('checked'))
+     let idCard = $(this).attr('id').substring(1);
+     $('#'+idCard).parent().remove();
+
+  });
+
+
 });
+
+
 
 function findTemperature(state, city) {
   $.ajax({
@@ -31,22 +71,58 @@ function findTemperature(state, city) {
           // write the data from API into HTML using div id's
           // $("#infoArea").html("<p>Current temperature in " + location + " " + state + " is: </p>");
           // $("#temp").html("<p>" + temp_f + "°F</p>")
-          console.log(temp_f)
+          createCard(state,city,temp_c,temp_f);
       }
   });
 
 }
 
-//makes the select work with materilize
-$(document).ready(function() {
-    $('select').material_select();
+
+
+function createCard(state, city , tempC, tempF){
+  realCity = city;
+  city = city.replace(/ /g,'');
+  let html = `<div class='col s12 m3'> <div data-f='${tempF}' data-c='${tempC}' class='card blue-grey darken-1' id='${state+city}'> <div class='card-content white-text'> <span class='card-title'> ${state}, ${realCity}</span>
+                            <h2 id='h2${state+city}'>${tempF}°F</h2>
+                            <div class="switch" id='${state+city}'>
+                                <label>
+                                  °F
+                                  <input type="checkbox" id='check${state+city}'>
+                                  <span class="lever"></span>
+                                  °C
+                                </label>
+                            </div>
+                        </div>
+                        <div class="card-action">
+                            <a id='a${state+city}'>close</a>
+                        </div>
+                    </div>
+                </div>`
+  $('#temperatures').append(html);
+  $('span.lever').click(function() {
+     // let ($('#').prop('checked'))
+     let idCard = $(this).parent().parent().attr('id');
+     let checked = $('#check'+idCard).prop('checked');
+
+     if (checked) {
+        $('#h2'+idCard).text($('#'+idCard).data('f')+"°F");
+     }
+     else {
+        $('#h2'+idCard).text($('#'+idCard).data('c')+"°C");
+     }
+
+  });
+  $('a').click(function() {
+     // let ($('#').prop('checked'))
+     let idCard = $(this).attr('id').substring(1);
+     $('#'+idCard).parent().remove();
+
   });
 
 }
 
-var state_json = $.getJSON("/states.json")
-var $select = $('#state_select');
-$(state_json).each(function (index, option) {    
-    var $option = $("<option/>").attr("value", option.abbreviation).text(option.name);
-    $select.append($option);
-}
+
+
+
+
+
